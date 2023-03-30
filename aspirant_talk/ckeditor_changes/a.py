be992cd91e82faaabf779b2669043308aef4a9e1
ashutosh2807 <56646405+ashutosh2807@users.noreply.github.com>
2023-03-30 11:48:56 +0530

    Initial Django App

--- aspirant_talk/ckeditor_changes/a.py
+++ aspirant_talk/ckeditor_changes/a.py
@@ -2,11 +2,11 @@
 def movefile():
     import os
     import shutil
-    with open('/workspace/aspirant_talk/ckeditor_changes/a.txt','r') as firstfile, open('config.js','w+') as secondfile:
+    with open('/workspace/aspirant_talk/ckeditor_changes/a.txt','r') as firstfile, open('config.js','w') as secondfile:
         # read content from first file
         for line in firstfile:
 
                  # append content to second file
                  secondfile.write(line)
 
-    shutil.move("config.js", "/workspace/.heroku/python/lib/python3.10/site-packages/ckeditor/static/ckeditor/ckeditor")
+    shutil.move("config.js", "/workspace/.heroku/python/lib/python3.10/site-packages/ckeditor/static/ckeditor/ckeditor/config.js")
