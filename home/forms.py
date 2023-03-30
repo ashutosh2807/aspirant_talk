638ed0018312c485e9804fb66a9bcfe83253f3bf
ashutosh2807 <56646405+ashutosh2807@users.noreply.github.com>
2023-03-30 09:50:58 +0530

    Initial Django App

--- home/forms.py
+++ home/forms.py
@@ -1,4 +1,11 @@
-# from django.forms import forms
-# from django.contrib.auth.models import User
-
+from django import forms
+from django.contrib.auth.models import User
+from .models import *
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 # class 
+
+class blogForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+    class Meta:
+        model = Blog
+        fields = "__all__"
--- home/views.py
+++ home/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render,HttpResponse
 from .models import *
-# Create your views here.
+from .forms import *
+from django.views.generic import TemplateView
+from django.views.generic.edit import FormView
+
+
 def home(request):
     data = Blog.objects.all()
     context = {
         'data':data
     }
     return render(request,'home.html',context)
+
+
+class Blogview(FormView):
+    initial = {
+    
+    }
+    
+    template_name = 'register.html'
+    form_class = blogForm
+    success_url = '/'
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_heading'] = 'Register a Blog'
+        return context
+
+    def form_valid(self, form):
+        form.save(commit=True)
+        return super().form_valid(form)
