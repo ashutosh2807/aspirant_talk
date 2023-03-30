bd5985f5f3d226458f9326a967d83c0037b2f22f
ashutosh2807 <56646405+ashutosh2807@users.noreply.github.com>
2023-03-30 11:34:34 +0530

    Initial Django App

--- aspirant_talk/urls.py
+++ aspirant_talk/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from .ckeditor_changes.a import *
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-
+# movefile()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
